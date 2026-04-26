544afcfc8ca793474ece7a7feb1f6ce115b5e82b
aleksafon94 <aleksafon94@gmail.com>
2026-04-26 19:23:08 +0200

    Refs #133 - Added LoggingBehavior.cs as MediatR pipeline behavior

--- DocIntel.Application/DependencyInjection.cs
+++ DocIntel.Application/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using DocIntel.Application.Common.Behaviors;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DocIntel.Application;
@@ -7,6 +9,9 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AssemblyMarker).Assembly));
+        
+        //Pipeline behaviors
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
         return services;
     }
